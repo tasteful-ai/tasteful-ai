b84f0d3a709e5aa937f440187266d7ea8b680592
sooyeon_heo <suzi97@naver.com>
2025-01-06 21:33:19 +0900

    Feat : 초기 데이터 설정

---
+++ src/test/java/com/example/tastefulai/TastefulAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tastefulai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TastefulAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
