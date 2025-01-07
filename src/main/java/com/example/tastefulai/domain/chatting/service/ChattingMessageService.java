bd30c4a961eb1e9434d1bab40a40c64b473b87a0
jiyoon <rlawldbs9128@gmail.com>
2025-01-08 01:01:35 +0900

    Feat : 채팅 메시지 조회 구현 -최근 50개의 메시지만 볼 수 있게 제한 -메시지 발신자의 닉네임과 메시지 내용을 리스트로 반환

--- src/main/java/com/example/tastefulai/domain/chatting/service/ChattingMessageService.java
+++ src/main/java/com/example/tastefulai/domain/chatting/service/ChattingMessageService.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -32,5 +33,15 @@ public class ChattingMessageService {
         ChattingMessage savedMessage = chattingMessageRepository.save(chattingMessage);
 
         return new ChattingMessageResponseDto(savedMessage.getSenderNickname(), savedMessage.getMessage());
+    }
+
+    public List<ChattingMessageResponseDto> getMessages() {
+        Chattingroom chattingroom = chattingroomRepository.getSingleChattingroom();
+
+        List<ChattingMessage> messages = chattingMessageRepository.findTop50ByChattingroomOrderByCreatedAtDesc(chattingroom);
+
+        return messages.stream()
+                .map(message -> new ChattingMessageResponseDto(message.getSenderNickname(), message.getMessage()))
+                .collect(Collectors.toList());
     }
 }
