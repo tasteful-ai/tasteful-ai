61f13aa924cf643cb758770d874d7427f4c338a8
beunyeong <beunyeong.b@gmail.com>
2025-01-08 21:25:28 +0900

    fix : 코드 수정

--- src/main/java/com/example/tastefulai/global/common/dto/CommonListResponseDto.java
+++ src/main/java/com/example/tastefulai/global/common/dto/CommonListResponseDto.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 import java.util.List;
 
 @Getter
-public class CommonListResponseDto<T extends BaseDtoType> {
+public class CommonListResponseDto<T> {
 
     private final String message;
     private final List<T> data;
