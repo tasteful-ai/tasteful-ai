d3b9d6ff8cb9cbc97e0c5fe7955e26bb9712eaa4
정가현 <kahyun0608@gmail.com>
2025-01-10 02:57:33 +0900

    Fix : 경로에서 auth 삭제

--- src/main/java/com/example/tastefulai/domain/image/controller/ImageController.java
+++ src/main/java/com/example/tastefulai/domain/image/controller/ImageController.java
@@ -15,7 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
-@RestController("/api/auth/members/{memberId}/profiles")
+@RestController("/api/members/{memberId}/profiles/images")
 @RequiredArgsConstructor
 public class ImageController {
 
