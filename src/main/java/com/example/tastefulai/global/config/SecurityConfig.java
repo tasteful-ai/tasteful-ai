e027650bd0dd6701593415638915b4d7d6e2b8af
beunyeong <beunyeong.b@gmail.com>
2025-01-09 19:54:36 +0900

    Fix : Spring-security 관련 임시 주석 처리

--- src/main/java/com/example/tastefulai/global/config/SecurityConfig.java
+++ src/main/java/com/example/tastefulai/global/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.tastefulai.global.config;
 
-import com.example.tastefulai.global.config.filter.JwtAuthFilter;
 import com.example.tastefulai.global.constant.EndpointConstants;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -17,11 +16,11 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity  //@Configuration 어노테이션 포함 및 보안 기능 활성화
+//@EnableWebSecurity  //@Configuration 어노테이션 포함 및 보안 기능 활성화
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final JwtAuthFilter jwtAuthFilter;
+//    private final JwtAuthFilter jwtAuthFilter;
 
     // 비밀번호 암호화(BCryptPasswordEncoder) 등록
     @Bean
@@ -35,20 +34,22 @@ public class SecurityConfig {
     }
 
     // 애플리케이션 보안 필터 체인
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        DefaultSecurityFilterChain build = httpSecurity
-                .httpBasic().disable()
-                .csrf().disable()
-                .cors().and()
-                .authorizeHttpRequests()
-                .requestMatchers(EndpointConstants.AUTH_SIGNUP, EndpointConstants.AUTH_LOGIN).permitAll() // 회원가입과 로그인 요청은 인증 없이 접근 허용
-                .anyRequest().authenticated() // 그 외 요청 인증 필요
-                .and()
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .build();
-        return build;
-    }
+    // 스프링시큐리티 버전에 맞게 코드 작성 -> 람다식
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+//        DefaultSecurityFilterChain build = httpSecurity
+//                .httpBasic().disable()
+//                .csrf().disable()
+//                .cors().and()
+//                .authorizeHttpRequests()
+//                .requestMatchers(EndpointConstants.AUTH_SIGNUP, EndpointConstants.AUTH_LOGIN).permitAll() // 회원가입과 로그인 요청은 인증 없이 접근 허용
+//                .anyRequest().authenticated() // 그 외 요청 인증 필요
+//                .and()
+//                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                .build();
+//        return build;
+//    }
 }
+
--- src/main/java/com/example/tastefulai/global/config/filter/JwtAuthFilter.java
+++ src/main/java/com/example/tastefulai/global/config/filter/JwtAuthFilter.java
@@ -1,62 +1,62 @@
-package com.example.tastefulai.global.config.filter;
-
-import com.example.tastefulai.domain.member.entity.Member;
-import com.example.tastefulai.domain.member.repository.MemberRepository;
-import com.example.tastefulai.global.error.errorcode.ErrorCode;
-import com.example.tastefulai.global.error.exception.CustomException;
-import com.example.tastefulai.global.util.JwtProvider;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import java.io.IOException;
-
-@Component
-@RequiredArgsConstructor
-@Slf4j
-public class JwtAuthFilter extends OncePerRequestFilter {
-
-    private final JwtProvider jwtProvider;
-    private final MemberRepository memberRepository;
-
-    // 요청시 실행하는 메서드
-    @Override
-    protected void doFilterInternal(HttpServletRequest httpServletRequest,
-                                    HttpServletResponse httpServletResponse,
-                                    FilterChain filterChain) throws ServletException, IOException {
-        // 요청에서 JWT(토큰) 확인
-        String token = resolveToken(httpServletRequest);
-
-        // 토큰 및 유효 여부 확인
-        if (token != null) {
-            if (jwtProvider.validateToken(token)) {
-                String email = jwtProvider.getEmailFromToken(token);
-
-                Member member = memberRepository.findByEmail(email)
-                        .orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
-
-                UsernamePasswordAuthenticationToken authentication =
-                        new UsernamePasswordAuthenticationToken(member, null, member.getAuthorities());
-                SecurityContextHolder.getContext().setAuthentication(authentication);
-            } else {
-                throw new CustomException(ErrorCode.INVALID_TOKEN);
-            }
-        }
-
-        filterChain.doFilter(httpServletRequest, httpServletResponse);
-    }
-
-    // JWT(토큰) 추출 메서드
-    private String resolveToken(HttpServletRequest httpServletRequest) {
-        // 요청 Header를 통한 "Authorization" 값 추출
-        String bearerToken = httpServletRequest.getHeader("Authorization");
-        return (bearerToken != null && bearerToken.startsWith("Bearer ")) ? bearerToken.substring(7) : null;
-    }
-}
+//package com.example.tastefulai.global.config.filter;
+//
+//import com.example.tastefulai.domain.member.entity.Member;
+//import com.example.tastefulai.domain.member.repository.MemberRepository;
+//import com.example.tastefulai.global.error.errorcode.ErrorCode;
+//import com.example.tastefulai.global.error.exception.CustomException;
+//import com.example.tastefulai.global.util.JwtProvider;
+//import jakarta.servlet.FilterChain;
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.filter.OncePerRequestFilter;
+//
+//import java.io.IOException;
+//
+//@Component
+//@RequiredArgsConstructor
+//@Slf4j
+//public class JwtAuthFilter extends OncePerRequestFilter {
+//
+//    private final JwtProvider jwtProvider;
+//    private final MemberRepository memberRepository;
+//
+//    // 요청시 실행하는 메서드
+//    @Override
+//    protected void doFilterInternal(HttpServletRequest httpServletRequest,
+//                                    HttpServletResponse httpServletResponse,
+//                                    FilterChain filterChain) throws ServletException, IOException {
+//        // 요청에서 JWT(토큰) 확인
+//        String token = resolveToken(httpServletRequest);
+//
+//        // 토큰 및 유효 여부 확인
+//        if (token != null) {
+//            if (jwtProvider.validateToken(token)) {
+//                String email = jwtProvider.getEmailFromToken(token);
+//
+//                Member member = memberRepository.findByEmail(email)
+//                        .orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
+//
+//                UsernamePasswordAuthenticationToken authentication =
+//                        new UsernamePasswordAuthenticationToken(member, null, member.getAuthorities());
+//                SecurityContextHolder.getContext().setAuthentication(authentication);
+//            } else {
+//                throw new CustomException(ErrorCode.INVALID_TOKEN);
+//            }
+//        }
+//
+//        filterChain.doFilter(httpServletRequest, httpServletResponse);
+//    }
+//
+//    // JWT(토큰) 추출 메서드
+//    private String resolveToken(HttpServletRequest httpServletRequest) {
+//        // 요청 Header를 통한 "Authorization" 값 추출
+//        String bearerToken = httpServletRequest.getHeader("Authorization");
+//        return (bearerToken != null && bearerToken.startsWith("Bearer ")) ? bearerToken.substring(7) : null;
+//    }
+//}
