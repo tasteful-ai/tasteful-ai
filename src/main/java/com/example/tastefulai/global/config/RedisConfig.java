b48e871e95eaef56d5a0dd77d69df192ac3b1c02
jiyoon <rlawldbs9128@gmail.com>
2025-01-09 10:11:48 +0900

    Feat : Redis 설정 구현 -RedisConfig: Pub/Sub 메시지 브로커를 사용하기 위해 설정

--- src/main/java/com/example/tastefulai/global/config/RedisConfig.java
+++ src/main/java/com/example/tastefulai/global/config/RedisConfig.java
@@ -1,11 +1,14 @@
 package com.example.tastefulai.global.config;
 
+import com.example.tastefulai.domain.chatting.redis.RedisSubscriber;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -29,5 +32,15 @@ public class RedisConfig {
 
     public ZSetOperations<String, String> zSetOperations(RedisTemplate<String, String> redisTemplate) {
         return redisTemplate.opsForZSet();
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory("localhost", 6379);
+    }
+
+    @Bean
+    public MessageListenerAdapter messageListener(RedisSubscriber redisSubscriber) {
+        return new MessageListenerAdapter(redisSubscriber, "handleMessage");
     }
 }
