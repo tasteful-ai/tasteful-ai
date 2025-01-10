d559acd79c4475db8460211402868596f1362630
beunyeong <beunyeong.b@gmail.com>
2025-01-10 19:45:25 +0900

    Feat : 유효성 검증 로직 추가

--- src/main/java/com/example/tastefulai/domain/member/dto/PasswordVerifyRequestDto.java
+++ src/main/java/com/example/tastefulai/domain/member/dto/PasswordVerifyRequestDto.java
@@ -1,10 +1,18 @@
 package com.example.tastefulai.domain.member.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class PasswordVerifyRequestDto {
 
+    @NotBlank(message = "비밀번호를 입력해주세요")
+    @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
+    @Pattern(
+            regexp =  "^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*()-+=]).{8,}$",
+            message = "비밀번호는 대문자, 소문자, 숫자, 특수문자를 포함해야 합니다.")
     private final String password;
 
     public PasswordVerifyRequestDto(String password) {
