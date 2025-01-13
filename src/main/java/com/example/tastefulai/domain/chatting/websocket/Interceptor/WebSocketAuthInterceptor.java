46126809059afc8ad68e3044c4a47ad331fec93e
jiyoon <rlawldbs9128@gmail.com>
2025-01-13 20:25:21 +0900

    Comment : WebSocketAuthInterceptor 주석처리 및 사용X

--- src/main/java/com/example/tastefulai/domain/chatting/websocket/Interceptor/WebSocketAuthInterceptor.java
+++ src/main/java/com/example/tastefulai/domain/chatting/websocket/Interceptor/WebSocketAuthInterceptor.java
@@ -1,65 +1,62 @@
-package com.example.tastefulai.domain.chatting.websocket.Interceptor;
-
-import com.example.tastefulai.global.util.JwtProvider;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.server.ServerHttpRequest;
-import org.springframework.http.server.ServerHttpResponse;
-import org.springframework.http.server.ServletServerHttpRequest;
-import org.springframework.stereotype.Component;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.server.HandshakeInterceptor;
-
-import java.util.Map;
-
-@Component
-@RequiredArgsConstructor
-@Slf4j
-public class WebSocketAuthInterceptor implements HandshakeInterceptor {
-
-    private final JwtProvider jwtProvider;
-
-    @Override
-    public boolean beforeHandshake(ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse,
-                                   WebSocketHandler webSocketHandler, Map<String, Object> attributes) {
-
-        String token = extractToken(serverHttpRequest);
-
-        if (token != null && jwtProvider.validateToken(token)) {
-            String email = jwtProvider.getEmailFromToken(token);
-            attributes.put("email", email);
-            log.info("WebSocket 인증 성공: 이메일={}", email);
-            return true;
-        }
-        log.warn("WebSocket 인증 실패: {}", token);
-        return false;
-    }
-
-    @Override
-    public void afterHandshake(ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse,
-                               WebSocketHandler webSocketHandler, Exception exception) {
-
-        if (exception != null) {
-            log.error("WebSocket handshake 오류: {}", exception.getMessage());
-        }
-    }
-
-    private String extractToken(ServerHttpRequest serverHttpRequest) {
-        //Authorization 헤더에서 추출
-        String header = serverHttpRequest.getHeaders().getFirst("Authorization");
-        if (header != null && header.startsWith("Bearer")) {
-            return header.substring(7);
-        }
-
-        //URL 에서 추출
-        if (serverHttpRequest instanceof ServletServerHttpRequest servletServerHttpRequest) {
-            String token = servletServerHttpRequest.getServletRequest().getParameter("token");
-
-            if (token != null && token.startsWith("Bearer ")) {
-                return token.substring(7);
-            }
-        }
-
-        return null;
-    }
-}
+//package com.example.tastefulai.domain.chatting.websocket.Interceptor;
+//
+//import com.example.tastefulai.global.util.JwtProvider;
+//import jakarta.servlet.http.Cookie;
+//import jakarta.servlet.http.HttpServletRequest;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.http.server.ServerHttpRequest;
+//import org.springframework.http.server.ServerHttpResponse;
+//import org.springframework.http.server.ServletServerHttpRequest;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.socket.WebSocketHandler;
+//import org.springframework.web.socket.server.HandshakeInterceptor;
+//
+//import java.util.Map;
+//
+//@Component
+//@RequiredArgsConstructor
+//@Slf4j
+//public class WebSocketAuthInterceptor implements HandshakeInterceptor {
+//
+//    private final JwtProvider jwtProvider;
+//
+//    @Override
+//    public boolean beforeHandshake(ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse,
+//                                   WebSocketHandler webSocketHandler, Map<String, Object> attributes) {
+//
+//        if (serverHttpRequest instanceof ServletServerHttpRequest) {
+//            ServletServerHttpRequest servletServerHttpRequest = (ServletServerHttpRequest) serverHttpRequest;
+//            String token = extractTokenFromCookie(((ServletServerHttpRequest) serverHttpRequest).getServletRequest());
+//
+//            if (token != null && jwtProvider.validateToken(token)) {
+//                String email = jwtProvider.getEmailFromToken(token);
+//                attributes.put("email", email);
+//                log.info("WebSocket 인증 성공: 이메일={}", email);
+//                return true;
+//            }
+//        }
+//        log.warn("WebSocket 인증 실패: {}", serverHttpRequest.getHeaders());
+//        return false;
+//    }
+//
+//    @Override
+//    public void afterHandshake(ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse,
+//                               WebSocketHandler webSocketHandler, Exception exception) {
+//
+//        if (exception != null) {
+//            log.error("WebSocket handshake 오류: {}", exception.getMessage());
+//        }
+//    }
+//
+//    private String extractTokenFromCookie(HttpServletRequest httpServletRequest) {
+//        if (httpServletRequest.getCookies() != null) {
+//            for (Cookie cookie : httpServletRequest.getCookies()) {
+//                if ("access-token".equals(cookie.getName())) {
+//                    return cookie.getValue();
+//                }
+//            }
+//        }
+//        return null;
+//    }
+//}
--- src/main/java/com/example/tastefulai/domain/chatting/websocket/config/WebSocketConfig.java
+++ src/main/java/com/example/tastefulai/domain/chatting/websocket/config/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.example.tastefulai.domain.chatting.websocket.config;
 
-import com.example.tastefulai.domain.chatting.websocket.Interceptor.WebSocketAuthInterceptor;
+//import com.example.tastefulai.domain.chatting.websocket.Interceptor.WebSocketAuthInterceptor;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -13,22 +14,22 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    private final WebSocketAuthInterceptor webSocketAuthInterceptor;
+//    private final WebSocketAuthInterceptor webSocketAuthInterceptor;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         //메시지 브로커 설정
-        registry.enableSimpleBroker("/sub"); //클라이언트가 메시지를 보는 경로
+        registry.enableSimpleBroker("/sub", "/queue"); //클라이언트가 메시지를 보는 경로
         registry.setApplicationDestinationPrefixes("/pub"); //메시지를 서버로 보낼 때 사용하는 경로
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-chat") //websocket 사용 시 엔드포인트
-                .setAllowedOrigins("*") //모든 도메인 허용 -> 나중에 프론트엔드 도메인 주소만 허용
-                .addInterceptors(webSocketAuthInterceptor)
-                .withSockJS()//websocket이 지원되지 않으면 SockJS 사용
-                .setSuppressCors(true);
+                .setAllowedOrigins("*"); //모든 도메인 허용 -> 나중에 프론트엔드 도메인 주소만 허용
+//                .addInterceptors(webSocketAuthInterceptor);
+//                .withSockJS();//websocket이 지원되지 않으면 SockJS 사용
+//                .setSuppressCors(true);
     }
 
 }
