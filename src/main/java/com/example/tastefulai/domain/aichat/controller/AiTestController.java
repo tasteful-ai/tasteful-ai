61d623bd36bba087c2cf964d5479c23989c2d04a
sooyeon_heo <suzi97@naver.com>
2025-01-14 17:42:11 +0900

    Del : 사용하지 않는 AiTestController 주석 처리, 추후 삭제 예정

--- src/main/java/com/example/tastefulai/domain/aichat/controller/AiTestController.java
+++ src/main/java/com/example/tastefulai/domain/aichat/controller/AiTestController.java
@@ -9,20 +9,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/api/users/aiChats")
-public class AiTestController {
-
-    private final ChatClient chatClient;
-
-    @GetMapping("/ai")
-    Map<String, String> completion(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
-        return Map.of(
-                "completion",
-                chatClient.prompt()
-                        .user(message)
-                        .call()
-                        .content());
-    }
-}
+//@RestController
+//@RequiredArgsConstructor
+//@RequestMapping("/api/users/aiChats")
+//public class AiTestController {
+//
+//    private final ChatClient chatClient;
+//
+//    @GetMapping("/ai")
+//    Map<String, String> completion(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+//        return Map.of(
+//                "completion",
+//                chatClient.prompt()
+//                        .user(message)
+//                        .call()
+//                        .content());
+//    }
+//}
