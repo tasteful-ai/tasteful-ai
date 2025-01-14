c408a323b556cb10c912954ee0803ca3be415402
정가현 <kahyun0608@gmail.com>
2025-01-14 21:28:49 +0900

    Fix : 이미지 업로드, 삭제 시 로그인된 유저의 정보를 가져올 수 있도록 수정

--- src/main/java/com/example/tastefulai/domain/image/controller/ImageController.java
+++ src/main/java/com/example/tastefulai/domain/image/controller/ImageController.java
@@ -2,12 +2,14 @@ package com.example.tastefulai.domain.image.controller;
 
 import com.example.tastefulai.domain.image.dto.ImageResponseDto;
 import com.example.tastefulai.domain.image.service.ImageService;
+import com.example.tastefulai.domain.member.entity.Member;
 import com.example.tastefulai.global.common.dto.CommonResponseDto;
+import com.example.tastefulai.global.config.auth.MemberDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,7 +19,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/api/members/{memberId}/profiles/images")
+@RequestMapping("/api/members/profiles/images")
 @RequiredArgsConstructor
 public class ImageController {
 
@@ -27,19 +29,23 @@ public class ImageController {
     //이후 @PathVariable 의 memberId를 로그인한 유저의 정보로 받아오게 바꿀 것
     //ProfileResponseDto 의 tastes 바꿀것
     @PutMapping
-    public ResponseEntity<CommonResponseDto<ImageResponseDto>> updateImage(@PathVariable Long memberId,
+    public ResponseEntity<CommonResponseDto<ImageResponseDto>> updateImage(@AuthenticationPrincipal MemberDetailsImpl memberDetailsImpl,
                                                                            @RequestParam(required = false) MultipartFile image) throws IOException {
 
-        ImageResponseDto imageResponseDto = imageService.uploadImage(memberId, image);
+        Member member = memberDetailsImpl.getMember();
+
+        ImageResponseDto imageResponseDto = imageService.uploadImage(member, image);
 
         return new ResponseEntity<>(new CommonResponseDto<>("프로필 이미지 업데이트 성공", imageResponseDto), HttpStatus.OK);
     }
 
     // 프로필 사진 삭제
     @DeleteMapping
-    public ResponseEntity<CommonResponseDto<ImageResponseDto>> deleteImage(@PathVariable Long memberId) {
+    public ResponseEntity<CommonResponseDto<ImageResponseDto>> deleteImage(@AuthenticationPrincipal MemberDetailsImpl memberDetailsImpl) {
 
-        imageService.deleteImage(memberId);
+        Member member = memberDetailsImpl.getMember();
+
+        imageService.deleteImage(member);
 
         ImageResponseDto imageResponseDto = new ImageResponseDto(
                 "https://tasteful-ai-image-bucket.s3.ap-northeast-2.amazonaws.com/%E1%84%80%E1%85%B5%E1%84%87%E1%85%A9%E1%86%AB+%E1%84%8B%E1%85%B5%E1%84%86%E1%85%B5%E1%84%8C%E1%85%B5.png"
