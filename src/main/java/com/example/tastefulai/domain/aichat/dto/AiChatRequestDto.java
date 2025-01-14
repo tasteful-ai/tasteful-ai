b9885a41c94b433ad5f75f48d35099f7a29213a2
sooyeon_heo <suzi97@naver.com>
2025-01-14 17:46:35 +0900

    Fix : AiChatRequestDto, AiChatResponseDto 클래스를 Java Record로 변경

--- src/main/java/com/example/tastefulai/domain/aichat/dto/AiChatRequestDto.java
+++ src/main/java/com/example/tastefulai/domain/aichat/dto/AiChatRequestDto.java
@@ -1,11 +1,13 @@
-//package com.example.tastefulai.domain.aichat.dto;
-//
-//import lombok.Getter;
-//import lombok.RequiredArgsConstructor;
-//
-//@Getter
-//@RequiredArgsConstructor
-//public class AiChatRequestDto {
-//
-//    private final String message;
-//}
+package com.example.tastefulai.domain.aichat.dto;
+
+/*
+역직렬화 오류로 인해 AI 요청이 실패하는 오류 발생
+Record 클래스는 불변 데이터를 객체 간에 전달하는 작업을 간단하게 만듦.
+멤버 변수는 private final로 선언(dto 객체 특성 상 값 무결성 유지로 final 선언)
+필드 별 getter 자동 생성
+모든 멤버 변수를 인자로 하는 public 생성자 자동 생성
+record는 불변 데이터를 다루므로 생성자 실행 시 인스턴트 필드 수정 불가
+equals, hashcode, toString 자동 생성
+기본 생성자는 제공 불가, 직접 생성해야 함.
+*/
+public record AiChatRequestDto(String message) {}
