9639901a5e2bb7ce2817d2cf0bb4d21a4b7198f2
beunyeong <beunyeong.b@gmail.com>
2025-01-23 12:26:29 +0900

    Refactor : 불필요한 Transactional 어노테이션 삭제 및 회원가입 코드 리팩토링

--- src/main/java/com/example/tastefulai/domain/member/controller/MemberController.java
+++ src/main/java/com/example/tastefulai/domain/member/controller/MemberController.java
@@ -1,15 +1,12 @@
 package com.example.tastefulai.domain.member.controller;
 
 import com.example.tastefulai.domain.member.dto.*;
-import com.example.tastefulai.domain.member.enums.GenderRole;
-import com.example.tastefulai.domain.member.enums.MemberRole;
 import com.example.tastefulai.domain.member.service.MemberService;
 import com.example.tastefulai.global.common.dto.CommonResponseDto;
 import com.example.tastefulai.global.common.dto.JwtAuthResponse;
 import com.example.tastefulai.global.config.auth.MemberDetailsImpl;
 import com.example.tastefulai.global.error.errorcode.ErrorCode;
 import com.example.tastefulai.global.error.exception.CustomException;
-import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -28,17 +25,18 @@ public class MemberController {
     // 1. 회원가입
     @PostMapping("/signup")
     public ResponseEntity<CommonResponseDto<MemberResponseDto>> signup(@Valid @RequestBody MemberRequestDto memberRequestDto) {
-        String email = memberRequestDto.getEmail();
-        String password = memberRequestDto.getPassword();
-        String nickname = memberRequestDto.getNickname();
-        Integer age = memberRequestDto.getAge();
-        GenderRole genderRole = memberRequestDto.getGenderRole();
-        MemberRole memberRole = memberRequestDto.getMemberRole();
-
-        MemberResponseDto memberResponseDto = memberService.signup(email, password, nickname, age, genderRole, memberRole);
+        MemberResponseDto memberResponseDto = memberService.signup(
+                memberRequestDto.getMemberRole(),
+                memberRequestDto.getEmail(),
+                memberRequestDto.getPassword(),
+                memberRequestDto.getNickname(),
+                memberRequestDto.getAge(),
+                memberRequestDto.getGenderRole()
+        );
 
         return new ResponseEntity<>(new CommonResponseDto<>("회원가입 완료", memberResponseDto), HttpStatus.CREATED);
     }
+
 
     // 2. 로그인
     @PostMapping("/login")
@@ -46,6 +44,7 @@ public class MemberController {
         JwtAuthResponse jwtAuthResponse = memberService.login(loginRequestDto.getEmail(), loginRequestDto.getPassword());
         return new ResponseEntity<>(new CommonResponseDto<>("로그인 성공", jwtAuthResponse), HttpStatus.OK);
     }
+
 
     // 3. 로그아웃
     @PostMapping("/logout")
@@ -55,8 +54,8 @@ public class MemberController {
         return new ResponseEntity<>(new CommonResponseDto<>("로그아웃 완료", null), HttpStatus.OK);
     }
 
+
     // 4. 비밀번호 변경
-    @Transactional
     @PatchMapping("/passwords")
     public ResponseEntity<CommonResponseDto<Void>> updatePassword(@AuthenticationPrincipal MemberDetailsImpl memberDetailsImpl,
                                                                   @RequestHeader("Authorization") String authorizationHeader,
@@ -71,6 +70,7 @@ public class MemberController {
 
         return new ResponseEntity<>(new CommonResponseDto<>("비밀번호 변경 완료", null), HttpStatus.OK);
     }
+
 
     // 5. 비밀번호 검증
 //    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
@@ -90,8 +90,9 @@ public class MemberController {
         return new ResponseEntity<>(new CommonResponseDto<>("비밀번호 검증 완료", null), HttpStatus.OK);
     }
 
+
     // 6. 계정 삭제(사용자용)
-    @PreAuthorize("hasRole('USER') or hasRole('OWNER')")
+//    @PreAuthorize("hasRole('USER') or hasRole('OWNER')")
     @DeleteMapping("/members/{memberId}")
     public ResponseEntity<CommonResponseDto<Void>> deleteOwnAccount(@PathVariable Long memberId) {
 
