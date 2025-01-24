22fa5a267175c80ea393fc3d53afb9af78674d81
정가현 <kahyun0608@gmail.com>
2025-01-24 18:59:02 +0900

    Refactor : 사용하지 않는 findImageUrlByMemberId 메서드 삭제
    - Member 와 Image 의 양방향 연관관계 매핑으로 인해 필요없어진 메서드 삭제

--- src/main/java/com/example/tastefulai/domain/image/repository/ImageRepository.java
+++ src/main/java/com/example/tastefulai/domain/image/repository/ImageRepository.java
@@ -5,7 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
 
-    String findImageUrlByMemberId(Long memberId);
-
     Image findByMemberId(Long memberId);
 }
