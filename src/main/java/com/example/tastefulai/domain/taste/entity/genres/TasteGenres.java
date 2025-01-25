c0fd91a2b20db54839fc31b6aa07b8d95e6db1ee
sooyeon_heo <suzi97@naver.com>
2025-01-26 04:39:13 +0900

    Feat : Genres, LikeFoods, DisLikeFoods, DietaryPreferences, SpicyLevel 에 대한 중간 테이블 생성

---
+++ src/main/java/com/example/tastefulai/domain/taste/entity/genres/TasteGenres.java
@@ -0,0 +1,37 @@
+package com.example.tastefulai.domain.taste.entity.genres;
+
+import com.example.tastefulai.domain.member.entity.Member;
+import com.example.tastefulai.global.common.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Table;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "taste_genres")
+public class TasteGenres extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "genre_id")
+    private Genres genres;
+
+    public TasteGenres(Member member) {
+        this.member = member;
+    }
+}
---
+++ src/main/java/com/example/tastefulai/domain/taste/entity/likefoods/TasteLikeFoods.java
@@ -0,0 +1,37 @@
+package com.example.tastefulai.domain.taste.entity.likefoods;
+
+import com.example.tastefulai.domain.member.entity.Member;
+import com.example.tastefulai.global.common.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "taste_like_foods")
+public class TasteLikeFoods extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "like_food_id")
+    private LikeFoods likeFoods;
+
+    public TasteLikeFoods(Member member) {
+        this.member = member;
+    }
+}
