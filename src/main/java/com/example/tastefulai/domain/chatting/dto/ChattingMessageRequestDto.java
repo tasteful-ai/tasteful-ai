032daa4531966f5b55e5eb021210d2cc273550a7
jiyoon <rlawldbs9128@gmail.com>
2025-01-29 22:18:05 +0900

    Fix : 어노테이션 변경 -Long 타입 필드 유효성 검사 @NotBlank -> @NotNull로 변경

--- src/main/java/com/example/tastefulai/domain/chatting/dto/ChattingMessageRequestDto.java
+++ src/main/java/com/example/tastefulai/domain/chatting/dto/ChattingMessageRequestDto.java
@@ -1,6 +1,7 @@
 package com.example.tastefulai.domain.chatting.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,7 @@ public class ChattingMessageRequestDto {
     @Size(max = 255, message = "메시지는 최대 255자까지 가능합니다.")
     private String message;
 
-    @NotBlank(message = "채팅방 ID는 필수입니다.")
+    @NotNull(message = "채팅방 ID는 필수입니다.")
     private Long chattingroomId;
 
     public ChattingMessageRequestDto(String message, Long chattingroomId) {
