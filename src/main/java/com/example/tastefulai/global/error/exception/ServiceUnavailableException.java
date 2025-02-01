862c9847651f68a609e267f0111b78d02066f88a
sooyeon_heo <suzi97@naver.com>
2025-02-02 05:45:12 +0900

    Refactor : 503 SERVICE_UNAVAILABLE 에러를 429 TOO_MANY_REQUESTS로 변경

--- src/main/java/com/example/tastefulai/global/error/exception/ServiceUnavailableException.java
+++
@@ -1,8 +0,0 @@
-package com.example.tastefulai.global.error.exception;
-
-public class ServiceUnavailableException extends RuntimeException {
-
-    public ServiceUnavailableException(String message) {
-        super(message);
-    }
-}
