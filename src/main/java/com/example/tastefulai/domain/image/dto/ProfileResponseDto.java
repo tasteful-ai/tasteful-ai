24649772cbd878897ed254cea6daa098738f0b8c
정가현 <kahyun0608@gmail.com>
2025-02-03 02:08:19 +0900

    Fix : ProfileResponseDto 에 dislikeFoods 필드 추가

--- src/main/java/com/example/tastefulai/domain/image/dto/ProfileResponseDto.java
+++ src/main/java/com/example/tastefulai/domain/image/dto/ProfileResponseDto.java
@@ -27,24 +27,28 @@ public class ProfileResponseDto {
 
     private List<String> likeFoods;
 
+    private List<String> dislikeFoods;
+
     private List<String> dietaryPreferences;
 
-    private List<Integer> tasteSpicyLevels;
+    private List<Integer> spicyLevels;
 
     public ProfileResponseDto(String nickname,
                               String imageUrl,
                               LocalDate createdAt,
                               List<String> genres,
                               List<String> likeFoods,
+                              List<String> dislikeFoods,
                               List<String> dietaryPreferences,
-                              List<Integer> tasteSpicyLevels) {
+                              List<Integer> spicyLevels) {
         this.nickname = nickname;
         this.imageUrl = imageUrl;
         this.createdAt = createdAt;
         this.genres = genres;
         this.likeFoods = likeFoods;
+        this.dislikeFoods = dislikeFoods;
         this.dietaryPreferences = dietaryPreferences;
-        this.tasteSpicyLevels = tasteSpicyLevels;
+        this.spicyLevels = spicyLevels;
     }
 
     public static ProfileResponseDto fromMember(Member member) {
@@ -54,6 +58,7 @@ public class ProfileResponseDto {
                 member.getCreatedAt().toLocalDate(),
                 member.getTasteGenres().stream().map(tg->tg.getGenres().getGenreName()).toList(),
                 member.getTasteLikeFoods().stream().map(lf->lf.getLikeFoods().getLikeName()).toList(),
+                member.getTasteDislikeFoods().stream().map(df->df.getDislikeFoods().getDislikeName()).toList(),
                 member.getTasteDietaryPreferences().stream().map(dp->dp.getDietaryPreferences().getPreferenceName()).toList(),
                 member.getTasteSpicyLevels().stream().map(sl->sl.getSpicyLevel().getSpicyLevel()).toList()
         );
