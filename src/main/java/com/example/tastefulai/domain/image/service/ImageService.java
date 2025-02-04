6213f00838fa2b8a9709851511c179ce00b843c1
정가현 <kahyun0608@gmail.com>
2025-02-04 20:42:59 +0900

    Comment : 불필요한 throw 문 삭제 및 주석 삭제

--- src/main/java/com/example/tastefulai/domain/image/service/ImageService.java
+++ src/main/java/com/example/tastefulai/domain/image/service/ImageService.java
@@ -1,14 +1,12 @@
 package com.example.tastefulai.domain.image.service;
 
 import com.example.tastefulai.domain.image.dto.ImageResponseDto;
-import com.example.tastefulai.domain.member.entity.Member;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 
 public interface ImageService {
 
-    ImageResponseDto uploadImage(Long memberId, MultipartFile image) throws IOException;
+    ImageResponseDto uploadImage(Long memberId, MultipartFile image);
 
     void deleteImage(Long memberId);
 }
