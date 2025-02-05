9db1be680f2bd7144d5b256410ec655675c88ca1
beunyeong <beunyeong.b@gmail.com>
2025-02-06 03:00:03 +0900

    Refactor : ENUM 타입(GenderRole, MemberRole)으로 변경

--- src/main/java/com/example/tastefulai/domain/member/dto/MemberListResponseDto.java
+++ src/main/java/com/example/tastefulai/domain/member/dto/MemberListResponseDto.java
@@ -1,6 +1,8 @@
 package com.example.tastefulai.domain.member.dto;
 
 import com.example.tastefulai.domain.member.entity.Member;
+import com.example.tastefulai.domain.member.enums.GenderRole;
+import com.example.tastefulai.domain.member.enums.MemberRole;
 import lombok.Getter;
 
 import java.time.format.DateTimeFormatter;
@@ -12,19 +14,19 @@ public class MemberListResponseDto {
     private final String createdAt;
     private final String nickname;
     private final String email;
-    private final String gender;
-    private final String role;
+    private final GenderRole genderRole;
+    private final MemberRole memberRole;
     private final String deletedAt;
 
     public MemberListResponseDto (Long memberId, String createdAt, String nickname,
-                                  String email, String gender, String role,
+                                  String email, GenderRole genderRole, MemberRole memberRole,
                                   String deletedAt) {
         this.memberId = memberId;
         this.createdAt = createdAt;
         this.nickname = nickname;
         this.email = email;
-        this.gender = gender;
-        this.role = role;
+        this.genderRole = genderRole;
+        this.memberRole = memberRole;
         this.deletedAt = deletedAt;
     }
 
@@ -34,8 +36,8 @@ public class MemberListResponseDto {
                 member.getCreatedAt().format(DateTimeFormatter.ofPattern("yy/MM/dd")),
                 member.getNickname(),
                 member.getEmail(),
-                member.getGenderRole().name(),
-                member.getMemberRole().name(),
+                member.getGenderRole(),
+                member.getMemberRole(),
                 (member.getDeletedAt() != null) ? member.getDeletedAt().format(DateTimeFormatter.ofPattern("yy/MM/dd")) : null
         );
     }
