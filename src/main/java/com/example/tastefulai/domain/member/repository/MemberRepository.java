29856fd7b9ab049408f74a29a06b1583dbd16945
sooyeon_heo <suzi97@naver.com>
2025-02-08 04:59:05 +0900

    Fix : MemberRepository에 @Param 추가

--- src/main/java/com/example/tastefulai/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/tastefulai/domain/member/repository/MemberRepository.java
@@ -22,27 +22,27 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @Query("SELECT DISTINCT m FROM Member m " +
             "LEFT JOIN FETCH m.tasteGenres tg LEFT JOIN FETCH tg.genres " +
             "WHERE m.id = :id")
-    Optional<Member> findMemberWithTasteGenresById(Long member);
+    Optional<Member> findMemberWithTasteGenresById(@Param("id") Long id);
 
     @Query("SELECT DISTINCT m FROM Member m " +
             "LEFT JOIN FETCH m.tasteLikeFoods tl LEFT JOIN FETCH tl.likeFoods " +
             "WHERE m IN :member")
-    Optional<Member> findMemberWithTasteLikeFoodsByMember(Member member);
+    Optional<Member> findMemberWithTasteLikeFoodsByMember(@Param("member") Member member);
 
     @Query("SELECT DISTINCT m FROM Member m " +
             "LEFT JOIN FETCH m.tasteDislikeFoods td LEFT JOIN FETCH td.dislikeFoods " +
             "WHERE m IN :member")
-    Optional<Member> findMemberWithTasteDislikeFoodsByMember(Member member);
+    Optional<Member> findMemberWithTasteDislikeFoodsByMember(@Param("member") Member member);
 
     @Query("SELECT DISTINCT m FROM Member m " +
             "LEFT JOIN FETCH m.tasteDietaryPreferences tp LEFT JOIN FETCH tp.dietaryPreferences " +
             "WHERE m IN :member")
-    Optional<Member> findMemberWithTasteDietaryPreferencesByMember(Member member);
+    Optional<Member> findMemberWithTasteDietaryPreferencesByMember(@Param("member") Member member);
 
     @Query("SELECT DISTINCT m FROM Member m " +
             "LEFT JOIN FETCH m.tasteSpicyLevels ts LEFT JOIN FETCH ts.spicyLevel " +
             "WHERE m IN :member")
-    Optional<Member> findMemberWithTasteSpicyLevelsByMember(Member member);
+    Optional<Member> findMemberWithTasteSpicyLevelsByMember(@Param("member") Member member);
 }
 
 // 아래 코드는 성능 최적화 비교 테스트를 진행한 뒤 가장 빠른 최적화 코드로 수정하여 주석 삭제할 예정
